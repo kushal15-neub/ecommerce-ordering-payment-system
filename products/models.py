d970c982980d857258281d47d42ffb8411cbe7a4
kushal15-neub <kushalpantha@gmail.com>
2026-07-12 01:32:48 +0600

    Product Model and Register admin done

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    """
+    Product model for storing product information
+    """
+
+    name = models.CharField(max_length=255)
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    stock = models.PositiveIntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
