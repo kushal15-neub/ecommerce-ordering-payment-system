d7c39bc9c1da7b2efb06d7dd9b8af1e0f925dc28
kushal15-neub <kushalpantha@gmail.com>
2026-07-11 13:03:40 +0600

    Setup Django project and MySQL connection

--- config/settings.py
+++ config/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
